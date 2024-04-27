d02ed6b3e21ebb92b9595df3d047a252a59f764d
nuralazmi <nuralazmi@gmail.com>
2024-04-27 19:52:52 +0300

    append translated name to Exchange model and edited fillable

---
+++ api/app/Enums/LanguageFileNames.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\Enums;
+
+enum LanguageFileNames: string
+{
+    case CURRENCIES = 'currency_names';
+}
--- api/app/Models/Exchange.php
+++ api/app/Models/Exchange.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use app\Enums\LanguageFileNames;
+use Illuminate\Contracts\Foundation\Application as ApplicationAlias;
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Application;
 
 class Exchange extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'symbol',
+        'price',
+        'code',
+        'source',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $appends = ['translated_name'];
+
+    /**
+     * @return Application|array|string|Translator|ApplicationAlias|null
+     */
+    public function getTranslatedNameAttribute(): Application|array|string|Translator|ApplicationAlias|null
+    {
+        return trans(LanguageFileNames::CURRENCIES->value . '.' . $this->code);
+    }
 }
